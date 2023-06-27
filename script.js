acbb4b82f11422bd52d2569d2d669d62f4ddede3
pdor <pdor92@hotmail.it>
2023-06-27 18:29:32 +0200

    Added black pawns

--- script.js
+++ script.js
@@ -14,8 +14,21 @@ for (let i = 0; i < 64; i++) {
     chessboard.append(cell);
 }
 
+// piece creation functions
+function make_black_pawn(cell) {
+    let p = document.createElement("img");
+    p.src = "./img/pieces/Chess_pdt45.svg";
+    p.className = "piece";
+    document.getElementById(cell).append(p);
+}
+
+// add black pawns
+for (let i = 0; i < 8; i++) {
+    make_black_pawn(8+i);
+}
+
 const king = document.createElement("img");
 king.src = "./img/pieces/Chess_kdt45.svg";
-king.className = "black piece";
+king.className = "piece";
 
 document.getElementById("4").append(king);
