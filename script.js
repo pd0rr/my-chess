48a5fc1e28a2fdf319aeaa2f7bf6ce2f19d5a992
pdor <pdor92@hotmail.it>
2023-06-27 17:55:07 +0200

    Make chessboard black and white

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const chessboard = document.getElementById("chessboard");
 
 for (let i = 0; i < 64; i++) {
     let cell = document.createElement("div");
-    cell.className = "cell"
+    cell.className = "cell";
     cell.id = i;
+
+    if ((i + Math.floor(i / 8)) % 2 == 0) {
+        cell.classList.add("white");
+    } else {
+        cell.classList.add("black");
+    }
+
     chessboard.append(cell);
 }
