a6a7eb48b7d1b36f9531dd84c4f5594df4ba6dad
pdor <pdor92@hotmail.it>
2023-06-27 18:38:49 +0200

    Add all pieces to the board

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ for (let i = 0; i < 64; i++) {
 }
 
 // piece creation functions
+
+function make_piece(piece, color, cell) {
+    let p = document.createElement("img");
+    p.src = "./img/pieces/Chess_"+ piece + color + "t45.svg";
+    p.className = "piece";
+    document.getElementById(cell).append(p);
+}
+
 function make_black_pawn(cell) {
     let p = document.createElement("img");
     p.src = "./img/pieces/Chess_pdt45.svg";
+    p.className = "piece";
+    document.getElementById(cell).append(p);
+}
+
+function make_white_pawn(cell) {
+    let p = document.createElement("img");
+    p.src = "./img/pieces/Chess_plt45.svg";
     p.className = "piece";
     document.getElementById(cell).append(p);
 }
@@ -27,8 +42,25 @@ for (let i = 0; i < 8; i++) {
     make_black_pawn(8+i);
 }
 
-const king = document.createElement("img");
-king.src = "./img/pieces/Chess_kdt45.svg";
-king.className = "piece";
+// add white pawns
+for (let i = 0; i < 8; i++) {
+    make_white_pawn(48+i);
+}
 
-document.getElementById("4").append(king);
+make_piece("r", "d", 0);
+make_piece("n", "d", 1);
+make_piece("b", "d", 2);
+make_piece("q", "d", 3);
+make_piece("k", "d", 4);
+make_piece("b", "d", 5);
+make_piece("n", "d", 6);
+make_piece("r", "d", 7);
+
+make_piece("r", "l", 56);
+make_piece("n", "l", 57);
+make_piece("b", "l", 58);
+make_piece("q", "l", 59);
+make_piece("k", "l", 60);
+make_piece("b", "l", 61);
+make_piece("n", "l", 62);
+make_piece("r", "l", 63);
